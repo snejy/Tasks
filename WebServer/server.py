41ab6590080eda411b783e9df0b3ce24fef01fb2
snejy <snspasova@gmail.com>
2014-11-03 23:14:19 +0200

    fix get argument handler

--- WebServer/server.py
+++ WebServer/server.py
@@ -7,8 +7,11 @@ import sys
 import subprocess
 import os
 import base64
+import os.path
+from copy import deepcopy
 
 class MySSLHTTPServer(TCPServer):
+    
     def server_bind(self):
         TCPServer.server_bind(self)
         host, port = self.socket.getsockname()[:2]
@@ -17,8 +20,16 @@ class MySSLHTTPServer(TCPServer):
 
 
 class MyHandler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
         try:
+            parts = self.path.split("?")
+            self.path = deepcopy(parts[0])
+            arguments = []
+            if len(parts) > 1:
+                arguments = parts[1].split("&")
+                # print(arguments)
+
             if self.path.endswith(".html"):
                 f = open(curdir + sep + self.path) 
                 self.wfile.write(bytes(f.read(), 'utf-8'))
@@ -29,13 +40,11 @@ class MyHandler(BaseHTTPRequestHandler):
                 return
 
             if any(self.path.endswith(x) for x in [".jpeg", ".jpg", ".png"]):
-                with open(curdir + sep + self.path, 'rb') as img:
-                    encoded_string = base64.b64encode(img.read())
-                    self.wfile.write(bytes('<html><body>', 'utf-8'))
-                    self.wfile.write(encoded_string)
-                    print(self.path[1:])
-                    self.wfile.write(bytes('<img alt="Image" src="data:image/png;base64,<{}>" style="width:304px;height:228px">'.format(encoded_string),'utf-8'))
-                    self.wfile.write(bytes('</body></html>', 'utf-8'))
+
+                path_to_image = os.getcwd() + self.path
+                with open(path_to_image, 'rb') as image:
+                    self.wfile.write(image.read())
+
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
@@ -52,11 +61,21 @@ class MyHandler(BaseHTTPRequestHandler):
                     self.wfile.write(bytes(str(line)[2:len(line)+1], 'utf-8'))
 
             if any(self.path.endswith(x) for x in [".py",".pl",".rb",".exe",".sh"]):
-                binds = {".py" : "python", ".pl" : "perl", ".rb" : "ruby", ".exe" : "exec", ".sh" : "bash"}
-                p = subprocess.Popen(binds["." + self.path.split('.', 2)[-1]] + " " + os.getcwd() + self.path,
-                                           shell=True,
-                                           stdout=subprocess.PIPE,
-                                           stderr=subprocess.STDOUT)
+                binds = {".py" : "python3", ".pl" : "perl", ".rb" : "ruby", ".exe" : "exec", ".sh" : "bash"}
+
+                if len(arguments) == 0: 
+                    p = subprocess.Popen(binds["." + self.path.split('.', 2)[-1]] + " " + os.getcwd() + self.path,
+                                               shell=True,
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.STDOUT)
+
+                else:
+                    names = list(map(lambda s: s.split("="), arguments))
+                    values = [x[1] for x in names]
+                    p = subprocess.Popen(binds["." + self.path.split('.', 2)[-1]] + " " + os.getcwd() + self.path +  " "  + " ".join(values),
+                                               shell=True,
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.STDOUT)
 
                 for line in p.stdout.readlines():
                     print(line)
@@ -69,22 +88,27 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_error(404,'File Not Found: %s' % self.path)
 
     def do_POST(self):
-        if self.path == "/send": #upload.html -> send
-            form = cgi.FieldStorage(
-                fp=self.rfile, 
-                headers=self.headers,
-                environ={'REQUEST_METHOD':'POST',
-                         'CONTENT_TYPE':self.headers['Content-Type'],
-            })
-            x = form["x"].value
-            y = form["y"].value
-            print("Your number 1 is: %s" % x)
-            print("Your number 2 is: %s" % y)
-            self.send_response(200)
-            self.end_headers()
-            z = int(x) + int(y)
-            self.wfile.write(bytes("The result from adding {} and {} is {} !".format(x, y, z), 'utf-8'))
-            return
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(post_data)
+
+    # def do_POST(self):
+    #     if self.path == "/send": #sum.html -> send
+    #         form = cgi.FieldStorage(
+    #             fp=self.rfile, 
+    #             headers=self.headers,
+    #             environ={'REQUEST_METHOD':'POST',
+    #                      'CONTENT_TYPE':self.headers['Content-Type'],
+    #         })
+    #         x = form["x"].value
+    #         y = form["y"].value
+    #         print("Your number 1 is: %s" % x)
+    #         print("Your number 2 is: %s" % y)
+    #         self.send_response(200)
+    #         self.end_headers()
+    #         z = int(x) + int(y)
+    #         self.wfile.write(bytes("The result from adding {} and {} is {} !".format(x, y, z), 'utf-8'))
+    #         return
 
 
 def main():
