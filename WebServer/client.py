4bf78a626d2798c025d1bba5674972216547a0c3
snejy <snspasova@gmail.com>
2014-11-07 15:39:17 +0200

    fix indentation

--- WebServer/client.py
+++ WebServer/client.py
@@ -12,26 +12,19 @@ def test_with_sequential_connections():
     my_count = 0
     while True:
       client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
       print('connecting to %s port %s' % ADDR)
-
       client.connect((ADDR))
-
       filename = '/format_string.rb'
-
       fileobj = client.makefile('rw', 10)
       fileobj.write("GET "+ filename +" HTTP/1.0\n\n")
-
       buff = fileobj.readlines()
       for line in buff:
           print ("-----------------------")
           print ("I'm Connection : %r" % my_count)
           print ("Received : %s" % line)
           my_count = my_count + 1
-
-      # print("closing connection")
-      # client.close()
-
+      print("closing connection")
+      client.close()
 
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
